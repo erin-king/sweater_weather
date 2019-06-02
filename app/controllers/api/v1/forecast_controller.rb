40c3ec57dfa137ca8e1ef5bb5d90710c73d65c13
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-02 10:46:17 -0600

    Add route to forecast controller, add index action

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
   def show
+    geocode_service = GooglegeocodeService.new
+    results = geocode_service.get_lat_long(params[:location])
+    latitude = results[:lat]
+    longitude = results[:lng]
     # render json: ForecastSerializer.new(Forecast.find(params[:id])) ?
   end
 end
