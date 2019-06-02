c0e8df32b7cdcf8811183842e1da06f8d993bf37
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-02 09:13:24 -0600

    Refactored DarkskyService

--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -1,12 +1,19 @@
 class DarkskyService
   def get_forecast(latitude, longitude)
-    conn = Faraday.new("https://api.darksky.net/forecast/8fe83b28502ceb642f504597ba13bb26/39.7392358,-104.990251") do |f|
+    get_json("#{latitude},#{longitude}")
+  end
+
+  private
+
+  def conn
+    Faraday.new("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API']}/") do |f|
       f.adapter Faraday.default_adapter
     end
+  end
 
-    response = conn.get
-    results = JSON.parse(response.body, symbolize_names: true)
-# binding.pry
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
 
