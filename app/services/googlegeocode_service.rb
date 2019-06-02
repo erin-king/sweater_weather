7851b57ec031d6f430ddb99d7f0ebabcbdb54a0b
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-02 09:19:32 -0600

    Refactor GooglegeocodeService

--- app/services/googlegeocode_service.rb
+++ app/services/googlegeocode_service.rb
@@ -1,14 +1,19 @@
 class GooglegeocodeService
   def get_lat_long(criteria)
-    # conn = Faraday.new('https://maps.googleapis.com/maps/api') do |f|
-    conn = Faraday.new('https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=AIzaSyB1jFkCyqI0pMD35LsEhrZkmDDMulItGbc') do |f|
-      # f.params['key'] = ENV['GEOCODE_API']
-      # f.params['key'] = 'AIzaSyB1jFkCyqI0pMD35LsEhrZkmDDMulItGbc'
+    get_json("geocode/json?address=#{criteria}")
+  end
+
+  private
+
+  def conn
+    Faraday.new('https://maps.googleapis.com/maps/api/') do |f|
+      f.params['key'] = ENV['GEOCODE_API']
       f.adapter Faraday.default_adapter
     end
+  end
 
-    # response = conn.get("/geocode/json?address=#{criteria}")
-    response = conn.get
-    results = JSON.parse(response.body, symbolize_names: true)[:results][0][:geometry][:location]
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)[:results][0][:geometry][:location]
   end
 end
