dfe894591647b129377904e370e4c5eb5c77ef79
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-03 12:07:19 -0600

    The end. There were a lot of silly hang ups. I think I could have gotten a serialized response if I had more time or just did not get caught up in my silly mistakes

--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -1,7 +1,7 @@
 class AntipodeService
 
   def get_antipode_first_results(latitude, longitude)
-    conn = Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{@latitude}&long=#{@longitude}") do |f|
+    conn = Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{latitude}&long=#{longitude}") do |f|
       f.headers['api_key'] = ENV['AMYPODE_KEY']
       f.adapter Faraday.default_adapter
     end
@@ -9,20 +9,12 @@ class AntipodeService
     results = JSON.parse(response.body, symbolize_names: true)
   end
 
-  # def get_antipode_summary(latitude, longitude)
-  #   # coords = geocode_service.get_lat_long(location)
-  #
-  #   # latitude = coords[:lat]
-  #   # longitude = coords[:lng]
-  #   conn = Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{latitude}&long=#{longitude}") do |f|
-  #     f.headers['api_key'] = ENV['AMYPODE_KEY']
-  #     f.adapter Faraday.default_adapter
-  #   end
-  #   response = conn.get
-  #   results = JSON.parse(response.body, symbolize_names: true)
-  #   # results = {:data=>{:id=>"1", :type=>"antipode", :attributes=>{:lat=>-22.3193039, :long=>-65.8306389}}}
-  #   antipode_latitude = results[:data][:attributes][:lat]
-  #   antipode_longitude = results[:data][:attributes][:long]
-  #   forecast = darksky_service.get_forecast(antipode_latitude, antipode_longitude)
-  # end
+  private
+
+  def conn
+    conn = Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{latitude}&long=#{longitude}") do |f|
+      f.headers['api_key'] = ENV['AMYPODE_KEY']
+      f.adapter Faraday.default_adapter
+    end
+  end
 end
