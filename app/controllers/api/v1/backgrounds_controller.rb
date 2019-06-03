8862f1e70f8b4a39fbba80618eedcfbd36ac0d68
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-03 16:35:44 -0600

    Add hash format to background_image return, updated users#create for post

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
     facade = SweaterweatherFacade.new(params[:location])
-    render json: facade.background_image
+    render json: { background_url: facade.background_image }
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,35 @@
 class Api::V1::UsersController < ApplicationController
+
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def create
-binding.pry
+    user = User.new(user_params)
+    api_key = SecureRandom.urlsafe_base64.to_s
+    user.api_key = api_key
+    if user.save
+      render json: { "api_key": api_key }
+    else
+      render json: { status: "error", code: 401 }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
+
+# class Api::V1::UsersController < ApplicationController
+#   def create
+#     user = User.new(user_params)
+#     if user.save
+#       user_key = {api_key: SecureRandom.hex}
+#       user.update(user_key)
+#       render json: user_key, status: :created
+#     else
+#       render nothing: true, status: :bad_request
+#     end
+#   end
+#
+# end
