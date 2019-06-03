5188e4250e7dd8f4e9436ea807bc778434b4498b
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-03 09:46:38 -0600

    Add antipode_summary method to sweaterweatherfacade, returning json from amypode api

--- app/facades/sweaterweather_facade.rb
+++ app/facades/sweaterweather_facade.rb
@@ -18,6 +18,17 @@ class SweaterweatherFacade
     @_latitude_longitude ||= @geocode_service.get_lat_long(@location)
   end
 
+  def antipode_summary
+    @geocode_service.get_lat_long('hongkong')
+    conn = Faraday.new('http://amypode.herokuapp.com/api/v1/antipodes?lat=27&long=-82') do |f|
+      f.headers['api_key'] = ENV['AMYPODE_KEY']
+      f.adapter Faraday.default_adapter
+    end
+    response = conn.get
+    results = JSON.parse(response.body, symbolize_names: true)
+binding.pry
+  end
+
   private
 
   def initialize_services
