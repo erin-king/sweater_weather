951a902bf9340fdd21ce35b22e0291692bc66d3d
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-02 18:29:27 -0600

    Add ForecastSerializer, formated forecast data per spec

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
     facade = SweaterweatherFacade.new(params[:location])
-    render json: facade.forecast
+    render json: ForecastSerializer.new(Forecast.new(facade.forecast))
   end
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe 'Forecast API' do
   it 'sends forecast data' do
     get '/api/v1/forecast?location=denver,co'
-    
-    results = JSON.parse(response.body, symbolize_names: true)
+
+    results = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
     expect(response).to be_successful
     expect(results[:currently]).to have_key(:summary)
   end
