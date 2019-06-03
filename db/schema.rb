fbe91d524af34fae0ba98d2459f02987004ae0e5
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-03 14:14:38 -0600

    Migration to add api_key column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_122236) do
+ActiveRecord::Schema.define(version: 2019_06_03_201137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_122236) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
   add_foreign_key "favorites", "users"
